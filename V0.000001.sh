7a99a3dd1f96e64067370c035f26a5af5b7b2212
JuanMesaUY <44150423+JuanMesaUY@users.noreply.github.com>
2018-12-02 17:27:39 -0300

    usuarios

--- V0.000001.sh
+++ V0.000001.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Controlamos si se ingresaron o no parametros, si es 0 envia mensaje por salida estandard de errores, y envia codigo de error 3
-if [ $# -eq 0 ]
-then
-        echo "Cantidad de parametros erronea, por favor ingrese algiuin archivo a listar" >&2
-        exit 3
-fi
-
 # Verificamos si el primer parametro empieza con -, en caso de que si, comparamos y vemos si es -t, que es valido, en caso contrario mandamos mensaje de error
 LISTAR=0
 if [ `echo $1 | cut -c1` = "-" ]    #identificamos si empieza o no con -, si no empieza sigue normal porque es un archivo
@@ -21,34 +14,45 @@ then
         fi
 fi
 
+# Controlamos si se ingresaron o no parametros, si es 0 envia mensaje por salida estandard de errores, y envia codigo de error 3
+if [ $# -eq 0 ]
+then
+        echo "Cantidad de parametros erronea, por favor ingrese algun archivo a listar" >&2
+        exit 3
+fi
+
 #Desplegamos info de archivos
 EXISTE=0
 NOEXISTE=0
 for i in "$@"
 do
-        if [ -a $i ]
+        CA=`readlink -e  $i`        #Cargo en variable CA, el camino absoluto del archivo con el comando readlink cada vez que empieza a recorrer un nuevo elemento de la lista el FOR
+        if [ -a $CA ]                   #compruebo que exista el archivo
         then
-                EXISTE=$(($EXISTE + 1 ))
+                if ls -d $CA 1>/dev/null               #Compruebo que tenga permisos de lectura con el comando LS
+                then
+                        EXISTE=$(($EXISTE + 1 ))
 
-                echo "Archivo:"`readlink -e $i`   "Cantidad de Links:"`ls -dl $i | tr -s " " | cut -d" " -f2`
-                echo "Numero de inodo:"`ls -dli $i | tr -s " " | cut -d" " -f1` "    Sistema de archivos montado en:"FALTAESTO
-                echo "Tamaño:"FALTAESTO"   Permisos:"`ls -dl $i | tr -s " " | cut -c 2-10`
-                echo "Tipo de archivo:""   Dueño:"`ls -dl $i | tr -s " " | cut -d" " -f3`
-                echo "Grupo:"`ls -dl $i | tr -s " " | cut -d" " -f4`
-                echo "Usuarios:"`getent group \`ls -dl $i | tr -s " " | cut -d" " -f4\` | cut -d: -f1,4`              
+                        echo "Archivo: "$CA     "Cantidad de Links: "`ls -dl $CA | tr -s " " | cut -d" " -f2`
+                        echo "Numero de inodo: "`ls -dli $CA | tr -s " " | cut -d" " -f1` "     Sistema de archivos montado en: "FALTAESTO
+                        echo "Tamaño: "FALTAESTO "       Permisos: "`ls -dl $CA | tr -s " " | cut -c 2-10`
+                        echo "Tipo de archivo: "FALTAESTO "      Dueño: "`ls -dl $CA | tr -s " " | cut -d" " -f3`
+                        echo "Grupo: "`ls -dl $CA | tr -s " " | cut -d" " -f4` "        Usuarios: "`getent group \`ls -dl $CA | tr -s " " | cut -d" " -f4\` | cut -d: -f4`       
 
-                echo
-                echo "--------------------------------------------------------------------------"
-                echo
+                        echo
+                        echo "--------------------------------------------------------------------------"
+                        echo
+
+                else
+                        echo "No se tienen los permisos necesarios para acceder a la informacion del archivo: " $CA >&2
+                        echo "///"
+                        NOEXISTE=1
+                fi
 
         else   
-                echo "No existe en el sistema el archivo:" $i >&2
+                echo "No existe en el sistema el archivo: " $CA >&2
                 echo "*** " >&2
-                NOEXISTE=1
-
-                echo "No se tienen los permisos necesarios para acceder a la informacion del archivo: " `readlink -e $i` >&2
-                echo "///"
-                NOEXISTE=1
+                NOEXISTE=1          
         fi
 
 done
@@ -56,10 +60,12 @@ done
 if [ LISTAR = 1 ]
 then
         echo "+++++++++++++++++++++++++++++"
-        echo "Se han listado" $EXISTE "archivos."
+        echo "Se han listado " $EXISTE " archivos."
 fi
 
 if [ $NOEXISTE = 1 ]
 then   
         exit 1
+else   
+        exit 0
 fi
---
+++ creacionestructuraarchivos.sh
@@ -0,0 +1,63 @@
+mkdir -p /obligatorio/prueba
+mkdir -p /obligatorio/usuario1
+mkdir -p /obligatorio/usuario2
+mkdir -p /obligatorio/usuario2/personal
+mkdir -p /obligatorio/usuario3
+
+echo prueba1 > /obligatorio/prueba/arch1
+echo prueba2 > /obligatorio/prueba/arch2
+echo prueba3 > /obligatorio/usuario1/arch3
+echo prueba4 > /obligatorio/usuario1/arch4
+echo prueba5 > /obligatorio/usuario1/arch5
+echo prueba6 > /obligatorio/usuario2/arch6
+echo prueba7 > /obligatorio/usuario2/arch7
+echo prueba8 > /obligatorio/usuario2/arch8
+echo prueba9 > /obligatorio/usuario3/arch9
+echo prueba10 > /obligatorio/usuario3/arch10
+echo prueba11 > /obligatorio/usuario3/arch11
+echo prueba12 > /obligatorio/usuario2/personal/arch12
+echo prueba13 > /obligatorio/usuario2/personal/arch13
+echo prueba14 > /obligatorio/usuario2/personal/arch14
+
+useradd -m prueba
+useradd -m usuario1
+useradd -m usuario2
+useradd -m usuario3
+useradd -m usuario4
+useradd -m usuario5
+useradd -m usuario6
+
+groupadd grupo1
+groupadd grupo2
+groupadd grupo3
+
+usermod -G grupo1 usuario1
+usermod -G grupo2 usuario2
+usermod -G grupo3 usuario3
+usermod -G grupo3 usuario4
+usermod -G grupo2 usuario5
+usermod -G grupo3 usuario6
+
+chmod 600 /obligatorio/prueba/arch1
+chmod 660 /obligatorio/prueba/arch2
+chmod 640 /obligatorio/usuario1/arch3
+chmod 640 /obligatorio/usuario1/arch4
+chmod 440 /obligatorio/usuario1/arch5
+chmod 400 /obligatorio/usuario2/arch6
+chmod 400 /obligatorio/usuario2/arch7
+chmod 400 /obligatorio/usuario2/arch8
+chmod 606 /obligatorio/usuario3/arch9
+chmod 606 /obligatorio/usuario3/arch10
+chmod 400 /obligatorio/usuario3/arch11
+chmod 604 /obligatorio/usuario2/personal/arch12
+chmod 604 /obligatorio/usuario2/personal/arch13
+chmod 606 /obligatorio/usuario2/personal/arch14
+
+chgrp grupo1 [ /obligatorio/usuario1/arch5 /obligatorio/usuario2/arch7 /obligatorio/usuario3/arch9 /obligatorio/usuario2/personal/arch13 /obligatorio/usuario2/personal/arch12 ]
+chgrp grupo2 [ /obligatorio/prueba/arch2 /obligatorio/usuario1/arch4 ]
+chgrp grupo3 [ /obligatorio/usuario3/arch10 /obligatorio/usuario2/personal/arch14 /obligatorio/prueba/arch1 /obligatorio/usuario1/arch3 ]
+
+chown -R prueba /obligatorio/prueba/
+chown -R usuario1 /obligatorio/usuario1/
+chown -R usuario2 /obligatorio/usuario2/
+chown -R usuario3 /obligatorio/usuario3/
